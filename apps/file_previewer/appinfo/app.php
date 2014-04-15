cc62ad6065c175d309419f8197404ae7e52815c0
Lloyd <l.harischandra@uws.edu.au>
2014-04-15 14:26:33 +1000

    added copyright details to file previewer app

--- apps/file_previewer/appinfo/app.php
+++ apps/file_previewer/appinfo/app.php
@@ -1,4 +1,26 @@
 <?php
+
+/**
+ * ownCloud - Cr8it App
+ *
+ * @author Lloyd Harischandra
+ * @copyright 2014 University of Western Sydney www.uws.edu.au
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 //add 3rdparty folder to include path
 $dir = dirname(dirname(__FILE__)).'/3rdparty';
 set_include_path(get_include_path() . PATH_SEPARATOR . $dir);

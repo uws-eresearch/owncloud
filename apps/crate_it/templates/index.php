82dc89e93e99b99ab292f653fdbbf09bbc1c643a
Marc Ziani de Ferranti <mezdef@gmail.com>
2014-05-05 14:27:37 +1000

    CRATEIT-72: initial UI layout work

--- apps/crate_it/templates/index.php
+++ apps/crate_it/templates/index.php
@@ -1,124 +1,136 @@
-<!-- 
+<!--
   ownCloud - Cr8it App
- 
+
   @author Lloyd Harischandra
   @copyright 2014 University of Western Sydney www.uws.edu.au
- 
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
   License as published by the Free Software Foundation; either
   version 3 of the License, or any later version.
- 
+
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU AFFERO GENERAL PUBLIC LICENSE for more details.
- 
+
   You should have received a copy of the GNU Lesser General Public
   License along with this library.  If not, see <http://www.gnu.org/licenses/>.
- 
+
  -->
 
-<div id="container">
+ <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 
-<button id="help_button" style="float:right;"> ? </button>
+<div id="container" class="crateit">
 
-<div style="padding-top:20px; padding-left:5px;">
+  <div class="bar-actions">
     <form id="crate_input" method="get">
-        Create new crate:
-        <input type="text" id="create">
-        <input id="subbutton" type="submit" value="Submit">
+      Create new crate:
+      <input type="text" id="create">
+      <input id="subbutton" type="submit" value="Submit">
     </form>
-    <select id="crates">
-        <?php foreach($_['crates'] as $crate):?>
-        <option id="<?php echo $crate; ?>" value="<?php echo $crate; ?>" <?php if($_['selected_crate']==$crate){echo 'selected';}?>>
-            <?php echo $crate;?>
-        </option>
-        <?php endforeach;?>
-    </select>
-    <input id="delete" type="button" value="Delete Crate" />
-    <input id="clear" type="button" value="Clear Crate" />
-    <?php if ($_['previews']==="on" ):?>
-    <input id="epub" type="button" value="EPUB" />
-    <?php endif; ?>
-    <input id="download" type="button" value="Download Crate as zip" />
-    <?php if ($_['sword_status'] === "enabled" ):?>
-        <div>
-            <select id="sword_collection">
-                <?php foreach ($_['sword_collections'] as $collection => $href): ?>
-                    <option value="<?php echo $href?>">
-                        <?php echo $collection; ?>
-                    </option>
-                <?php endforeach; ?>
-            </select>
-            <input id="post" type="button" value="Post Crate to SWORD" />
 
-    <?php endif; ?>
-</div>
+    <div class="pull-right">
+      <?php if ($_['previews']==="on" ):?>
+        <input id="epub" type="button" value="EPUB" />
+      <?php endif; ?>
+      <input id="download" type="button" value="Download Crate as zip" />
+
+      <input id="clear" type="button" value="Clear Crate" />
+      <a id="delete" type="button" class="button">
+        <i class="fa fa-trash-o"></i>
+      </a>
+
+      <button id="help_button"> Help? </button>
+    </div>
+
+  </div>
+
+  <div style="padding-top:20px; padding-left:5px;">
+    <select id="crates">
+      <?php foreach($_['crates'] as $crate):?>
+        <option id="<?php echo $crate; ?>" value="<?php echo $crate; ?>" <?php if($_['selected_crate']==$crate){echo 'selected';}?>>
+          <?php echo $crate;?>
+        </option>
+      <?php endforeach;?>
+    </select>
+    <?php if ($_['sword_status'] === "enabled" ):?>
+      <div>
+        <select id="sword_collection">
+          <?php foreach ($_['sword_collections'] as $collection => $href): ?>
+            <option value="<?php echo $href?>">
+              <?php echo $collection; ?>
+            </option>
+          <?php endforeach; ?>
+        </select>
+        <input id="post" type="button" value="Post Crate to SWORD" />
+
+      <?php endif; ?>
+    </div>
 
     <div id="metadata" style="float:right;">
-        <?php if ($_['mint_status'] === "enabled" ):?>
+      <?php if ($_['mint_status'] === "enabled" ):?>
 
-<!--         
-    		<div id="anzsrc_for">
-    		    <div>
-    		        <select id="for_top_level" class="for_codes">
-    		            <option id="select_top" value="for_top_choose">Choose a code</option>
-    		            <?php foreach($_['top_for'] as $item): $vars=get_object_vars($item); //$prefLabel=$ vars[ 'skos:prefLabel']; ?>
-    		            <option id="<?php echo $vars['rdf:about'];?>" value="<?php echo $vars['rdf:about'];?>">
-    		                <?php echo $vars[ 'skos:prefLabel']?>
-    		            </option>
-    		            <?php endforeach;?>
-    		        </select>
-    		    </div>
-    		    <div>
-    		        <select id="for_second_level" class="for_codes">
-    		            <option id="select_second" value="for_second_choose">Choose a code</option>
-    		        </select>
-    		    </div>
-    		    <div>
-    		        <select id="for_third_level" class="for_codes">
-    		            <option id="select_third" value="for_third_choose">Choose a code</option>
-    		        </select>
-    		    </div>
-    		</div>
- -->
- 
-    		<div id="creators_box">
-    		    <div>
-    		        <label for="creators">Add Data Creator/s</label>
-    		    </div>
-    		    <ul id="creators">
-                        <?php foreach($_['creators'] as $creator):?>
-    		    	  <li>
-    			    <input id="creator_<?php echo $creator['creator_id'] ?>" type="button" value="Remove" />
-    			    <span id="<?php echo $creator['creator_id'] ?>" class="full_name"><?php echo $creator['full_name'] ?></span>
-    			  </li>
-                        <?php endforeach;?>
-    		    </ul>
-    		</div>
+        <!--
+        <div id="anzsrc_for">
+        <div>
+        <select id="for_top_level" class="for_codes">
+        <option id="select_top" value="for_top_choose">Choose a code</option>
+        <?php foreach($_['top_for'] as $item): $vars=get_object_vars($item); //$prefLabel=$ vars[ 'skos:prefLabel']; ?>
+        <option id="<?php echo $vars['rdf:about'];?>" value="<?php echo $vars['rdf:about'];?>">
+        <?php echo $vars[ 'skos:prefLabel']?>
+      </option>
+    <?php endforeach;?>
+  </select>
+</div>
+<div>
+<select id="for_second_level" class="for_codes">
+<option id="select_second" value="for_second_choose">Choose a code</option>
+</select>
+</div>
+<div>
+<select id="for_third_level" class="for_codes">
+<option id="select_third" value="for_third_choose">Choose a code</option>
+</select>
+</div>
+</div>
+-->
 
-    		<div id="search_people_box">
-    		    <input id="keyword" type="text" name="keyword" />
-    		    <input id="search_people" type="button" value="Search People" />
-    		</div>
+<div id="creators_box">
+  <div>
+    <label for="creators">Add Data Creator/s</label>
+  </div>
+  <ul id="creators">
+    <?php foreach($_['creators'] as $creator):?>
+      <li>
+        <input id="creator_<?php echo $creator['creator_id'] ?>" type="button" value="Remove" />
+        <span id="<?php echo $creator['creator_id'] ?>" class="full_name"><?php echo $creator['full_name'] ?></span>
+      </li>
+    <?php endforeach;?>
+  </ul>
+</div>
 
-    		<div id="search_people_result_box">
-    		    <ul id="search_people_results">
-    		    </ul>
-    		</div>
+<div id="search_people_box">
+  <input id="keyword" type="text" name="keyword" />
+  <input id="search_people" type="button" value="Search People" />
+</div>
 
-		<div id="avtivities_box">
-		    <div>
-		        <label for="activities">Add Grant Number/s</label>
-		    </div>
-		    <ul id="activities">
-                    <?php foreach($_['activities'] as $activity):?>
-		    	  <li>
-			    <input id="activity_<?php echo $activity['activity_id'] ?>" type="button" value="Remove" />
-			    <span id="<?php echo $activity['activity_id'] ?>" class="grant_number"><?php echo $activity['grant_number'] ?></span>
-			  </li>
-                    <?php endforeach;?>
+<div id="search_people_result_box">
+  <ul id="search_people_results">
+  </ul>
+</div>
+
+<div id="avtivities_box">
+  <div>
+    <label for="activities">Add Grant Number/s</label>
+  </div>
+  <ul id="activities">
+    <?php foreach($_['activities'] as $activity):?>
+      <li>
+        <input id="activity_<?php echo $activity['activity_id'] ?>" type="button" value="Remove" />
+        <span id="<?php echo $activity['activity_id'] ?>" class="grant_number"><?php echo $activity['grant_number'] ?></span>
+      </li>
+    <?php endforeach;?>
 		    </ul>
 		</div>
 
@@ -185,7 +197,7 @@
             <li>Select your crate name from the <b><i>default_crate</i></b> dropdown menu</li>
         </ul>
     </p>
-    
+
     <p>
         <b>Describe Your Crate</b>
         <ul>
@@ -224,5 +236,3 @@
     <span id="max_sword_mb"><?php echo $_['max_sword_mb'] ?></span>
     <span id="max_zip_mb"><?php echo $_['max_zip_mb'] ?></span>
 </div>
-
-

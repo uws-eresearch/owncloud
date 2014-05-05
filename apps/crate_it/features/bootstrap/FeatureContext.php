1cdb7d1f0f3e8cc7943ec22cdf7f4dd02c784af7
Ilya Anisimoff <ilya@intersect.org.au>
2014-05-05 14:57:12 +1000

    MISC: Adds Mink

--- apps/crate_it/features/bootstrap/FeatureContext.php
+++ apps/crate_it/features/bootstrap/FeatureContext.php
@@ -53,4 +53,149 @@ class FeatureContext extends BehatContext
     {
         throw new PendingException();
     }
+
+       /**
+     * @Given /^I have file "([^"]*)" within the root folder$/
+     */
+    public function iHaveFileWithinTheRootFolder($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^I have folder "([^"]*)" within the root folder$/
+     */
+    public function iHaveFolderWithinTheRootFolder($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^I have folder "([^"]*)" within "([^"]*)"$/
+     */
+    public function iHaveFolderWithin($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^I have file "([^"]*)" within "([^"]*)"$/
+     */
+    public function iHaveFileWithin($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When /^I add a "([^"]*)" within the root folder to the default crate$/
+     */
+    public function iAddAWithinTheRootFolderToTheDefaultCrate($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then /^the default crate should contain "([^"]*)" in the root folder$/
+     */
+    public function theDefaultCrateShouldContainInTheRootFolder($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When /^I add "([^"]*)" within the root folder to the default crate$/
+     */
+    public function iAddWithinTheRootFolderToTheDefaultCrate($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then /^the default crate should contain "([^"]*)" within the root folder$/
+     */
+    public function theDefaultCrateShouldContainWithinTheRootFolder($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^the default crate should contain "([^"]*)" within "([^"]*)"$/
+     */
+    public function theDefaultCrateShouldContainWithin($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then /^"([^"]*)" should not be visible in the default crate$/
+     */
+    public function shouldNotBeVisibleInTheDefaultCrate($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When /^I expand the root folder in the default crate$/
+     */
+    public function iExpandTheRootFolderInTheDefaultCrate()
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then /^"([^"]*)" should be visible in the default crate$/
+     */
+    public function shouldBeVisibleInTheDefaultCrate($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When /^I add a "([^"]*)" to the default crate$/
+     */
+    public function iAddAToTheDefaultCrate($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then /^the default crate should contain "([^"]*)" within the root folder, in that order$/
+     */
+    public function theDefaultCrateShouldContainWithinTheRootFolderInThatOrder($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When /^I add "([^"]*)" to the default crate$/
+     */
+    public function iAddToTheDefaultCrate($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When /^I add folder "([^"]*)" within "([^"]*)" to the default crate$/
+     */
+    public function iAddFolderWithinToTheDefaultCrate($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given /^the default crate should not contain "([^"]*)" within the root folder$/
+     */
+    public function theDefaultCrateShouldNotContainWithinTheRootFolder($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When /^I delete the default crate$/
+     */
+    public function iDeleteTheDefaultCrate()
+    {
+        throw new PendingException();
+    }
+
 }

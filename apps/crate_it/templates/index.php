27d19badc1066535d8482ee862cb683b7a864434
veronica <veronica@intersect.org.au>
2014-05-29 16:23:53 +1000

    CRATEIT-45 Added tests for create crate feature

--- apps/crate_it/templates/index.php
+++ apps/crate_it/templates/index.php
@@ -330,24 +330,7 @@
   </div>
 </div>
 
-<div class="modal" id="newCrateModal" tabindex="-1" role="dialog" aria-labelledby="newCrateModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="newCrateModalLabel">New Crate</h4>
-      </div>
-      <div class="modal-body">
-        <p>New Cr8 Name</p>
-        <input id="crate_input_name" type="text"></input>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-        <button id="create_crate_submit" type="button" class="btn btn-primary">Create</button>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
+<?php include("create_crate_modal.php"); ?>
 
 <div class="modal" id="removeCrateModal" tabindex="-1" role="dialog" aria-labelledby="removeCrateModalLabel" aria-hidden="true">
   <div class="modal-dialog">

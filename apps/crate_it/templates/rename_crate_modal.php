adb6a60c958605601b1282b18a28663f24565abd
Ilya Anisimoff <ilya@intersect.org.au>
2014-06-03 16:29:30 +1000

    MISC: Extracts rename modal to separate template

---
+++ apps/crate_it/templates/rename_crate_modal.php
@@ -0,0 +1,18 @@
+<div class="modal" id="renameCrateModal" tabindex="-1" role="dialog" aria-labelledby="renameCrateModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+        <h4 class="modal-title" id="renameCrateModalLabel">Remove Item</h4>
+      </div>
+      <div class="modal-body">
+        <p>New Name:</p>
+        <input id="rename-item" type="text"></input>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+        <button type="button" class="btn btn-primary">Rename</button>
+      </div>
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->

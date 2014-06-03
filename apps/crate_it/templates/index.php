13bc2e68975d298f4b7af9cefbf418da259a241b
Ilya Anisimoff <ilya@intersect.org.au>
2014-06-03 16:37:28 +1000

    MISC: Extracts publish modal to separate template

--- apps/crate_it/templates/index.php
+++ apps/crate_it/templates/index.php
@@ -254,60 +254,7 @@
     <span id="max_zip_mb"><?php echo $_['max_zip_mb'] ?></span>
 </div>
 
-<div class="modal" id="publishModal" tabindex="-1" role="dialog" aria-labelledby="publishModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h2 class="modal-title" id="publishModalLabel">Publish Cr8t</h2>
-      </div>
-      <div class="modal-body">
-        <select id="sword_collection">
-          <?php foreach ($_['sword_collections'] as $collection => $href): ?>
-            <option value="<?php echo $href?>">
-              <?php echo $collection; ?>
-            </option>
-          <?php endforeach; ?>
-        </select>
-
-        <section>
-          <h3>Creators</h3>
-          <ul id="creators">
-            <?php foreach($_['creators'] as $creator):?>
-              <li>
-                <input id="creator_<?php echo $creator['creator_id'] ?>" type="button" value="Remove" />
-                <span id="<?php echo $creator['creator_id'] ?>" class="full_name"><?php echo $creator['full_name'] ?></span>
-              </li>
-            <?php endforeach;?>
-          </ul>
-        </section>
-
-        <section>
-          <h3>Grant Numbers</h3>
-          <ul id="activities">
-            <?php foreach($_['activities'] as $activity):?>
-              <li>
-                <input id="activity_<?php echo $activity['activity_id'] ?>" type="button" value="Remove" />
-                <span id="<?php echo $activity['activity_id'] ?>" class="grant_number"><?php echo $activity['grant_number'] ?></span>
-              </li>
-            <?php endforeach;?>
-          </ul>
-        </section>
-
-        <div id='#description_box'>
-          <label for="description">Description</label>
-          <input id="edit_description" type="button" value="Edit" />
-          <div id="description"><?php echo htmlentities($_['description']) ?></div>
-        </div>
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Publish</button>
-      </div>
-    </div>
-  </div>
-</div>
+<?php include 'publish_modal.php'; ?>
 
 <?php include 'create_crate_modal.php'; ?>
 

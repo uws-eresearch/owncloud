78c9f4d61a669cd88c93fad168b1df0ba603158d
Ilya Anisimoff <ilya@intersect.org.au>
2014-06-03 16:27:08 +1000

    MISC: Extracts remove crate modal to separate template

--- apps/crate_it/templates/index.php
+++ apps/crate_it/templates/index.php
@@ -309,25 +309,9 @@
   </div>
 </div>
 
-<?php include("create_crate_modal.php"); ?>
+<?php include 'create_crate_modal.php'; ?>
 
-<div class="modal" id="removeCrateModal" tabindex="-1" role="dialog" aria-labelledby="removeCrateModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="removeCrateModalLabel">Remove Item</h4>
-      </div>
-      <div class="modal-body">
-        <p>Remove item from crate?</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-        <button type="button" class="btn btn-primary">Remove</button>
-      </div>
-    </div><!-- /.modal-content -->
-  </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
+<?php include 'remove_crate_modal.php'; ?>
 
 <div class="modal" id="renameCrateModal" tabindex="-1" role="dialog" aria-labelledby="renameCrateModalLabel" aria-hidden="true">
   <div class="modal-dialog">

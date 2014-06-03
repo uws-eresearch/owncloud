36001eb19e4d028109df010171d543e7699ac8f0
Ilya Anisimoff <ilya@intersect.org.au>
2014-06-03 16:44:53 +1000

    MISC: Extracts javascript vars to a separate template

--- apps/crate_it/templates/index.php
+++ apps/crate_it/templates/index.php
@@ -195,15 +195,6 @@
     </div>
 
 
-
-
-<!-- workaround to make var avalaide to javascript -->
-<div id="hidden_vars" hidden="hidden">
-    <span id="description_length"><?php echo $_['description_length']; ?></span>
-    <span id="max_sword_mb"><?php echo $_['max_sword_mb'] ?></span>
-    <span id="max_zip_mb"><?php echo $_['max_zip_mb'] ?></span>
-</div>
-
 <?php include 'help_modal.php'; ?>
 
 <?php include 'publish_modal.php'; ?>
@@ -215,3 +206,5 @@
 <?php include 'rename_crate_modal.php'; ?>
 
 <?php include 'add_folder_modal.php'; ?>
+
+<?php include 'javascript_vars.php'; ?>

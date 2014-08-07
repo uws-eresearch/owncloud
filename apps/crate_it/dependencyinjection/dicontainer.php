cd4311ea248664784dab2753df8627defdbf53c8
Veronica Luke <veronica@intersect.org.au>
2014-08-07 15:16:39 +1000

    CRATEIT-60 Added DownloadController, DownloadService and ZipDownloadResponse.

--- apps/crate_it/dependencyinjection/dicontainer.php
+++ apps/crate_it/dependencyinjection/dicontainer.php
@@ -7,8 +7,10 @@ use \OCA\AppFramework\DependencyInjection\DIContainer as BaseContainer;
 use \OCA\crate_it\Controller\PageController;
 use \OCA\crate_it\Controller\CrateController;
 use \OCA\crate_it\Controller\SearchController;
+use \OCA\crate_it\Controller\DownloadController;
 use \OCA\crate_it\Service\CrateService;
 use \OCA\crate_it\Service\SetupService;
+use \OCA\crate_it\Service\DownloadService;
 use \OCA\crate_it\Manager\CrateManager;
 use \OCA\crate_it\Manager\ConfigManager;
 
@@ -41,6 +43,10 @@ class DIContainer extends BaseContainer {
             return new CrateService($c['API'], $c['CrateManager']);  
         };
         
+         $this['DownloadService'] = function($c) {
+            return new DownloadService($c['API']);  
+        };
+        
         /* Controllers */
                                
         $this['PageController'] = function($c) {
@@ -54,7 +60,10 @@ class DIContainer extends BaseContainer {
         $this['SearchController'] = function($c) {
             return new SearchController($c['API'], $c['Request'], $c['ConfigManager']);
         };
-
+        
+        $this['DownloadController'] = function($c) {
+            return new DownloadController($c['API'], $c['Request'], $c['DownloadService']);
+        };
     }
 
 }

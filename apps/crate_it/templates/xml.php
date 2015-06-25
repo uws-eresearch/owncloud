21dbaf9927ea3d9ddd1b6eacbd3fbed2d5713600
Ilya Anisimoff <ilya@intersect.org.au>
2015-06-25 12:05:11 +1000

    CRATEIT-MISC: Adds namespace to ReDBOX alert template

--- apps/crate_it/templates/xml.php
+++ apps/crate_it/templates/xml.php
@@ -1,25 +1,29 @@
-<crate>
-    <name><?php p($_['crate_name']) ?></name>
-    <date><?php p($_['created_date']) ?></date>
-    <location><?php print_unescaped($_['location']) ?></location>
-    <description><?php p($_['description']) ?></description>
+<?xml version="1.0" encoding="US-ASCII"?>
+<my:my:RedboxCollection xmlns:my="http://schemas.microsoft.com/office/infopath/2003/myXSD/2011-09-26T07:17:47"
+                     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                     xmlns:xd="http://schemas.microsoft.com/office/infopath/2003">
+<my:crate>
+    <my:name><?php p($_['crate_name']) ?></my:name>
+    <my:date><?php p($_['created_date']) ?></my:date>
+    <my:location><?php print_unescaped($_['location']) ?></my:location>
+    <my:description><?php p($_['description']) ?></my:description>
     <?php if ($_['creators']) { ?>
-        <creators>
+        <my:creators>
             <?php foreach ($_['creators'] as $creator) { ?>
-                <creator>
+                <my:creator>
                     <?php
-                    print_unescaped('<name>');
+                    print_unescaped('<my:name>');
                     p($creator['name']);
-                    print_unescaped('</name>');
-                    print_unescaped('<email>');
+                    print_unescaped('</my:name>');
+                    print_unescaped('<my:email>');
                     p($creator['email']);
-                    print_unescaped('</email>');
+                    print_unescaped('</my:email>');
                     ?>
-                </creator>
+                </my:creator>
             <?php } ?>
-        </creators>
+        </my:creators>
     <?php } ?>
-    <submitter>
-        <email><?php p($_['submitter']) ?></email>
-    </submitter>
-</crate>
+    <my:submitter>
+        <my:email><?php p($_['submitter']) ?></my:email>
+    </my:submitter>
+</my:crate>
